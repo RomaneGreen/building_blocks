c9b52229bd3967ad2ca6264f72098230768af0d5
Romane <romane71193@gmail.com>
2018-05-01 01:11:01 -0400

    substring file in substring

---
+++ substrings.rb
@@ -0,0 +1,28 @@
+
+
+
+
+def substrings(string,dictionary)
+
+  hashy = {}
+  sss = []
+  dictionary = ["am","is","the","can","we"]
+
+  dictionary.map do |w|
+
+
+  if string.include? w
+
+   sss.push("#{w}":"#{string.scan(/#{w}/).count}")
+
+  "#{w}" "=>" "#{string.scan(/#{w}/).count}"
+
+
+end
+
+
+end
+return sss.join(" , ").gsub(/[\"]/,"")
+
+
+end 
